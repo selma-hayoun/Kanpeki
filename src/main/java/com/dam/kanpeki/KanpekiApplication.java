1951f79364cba113edfeb3e595d1b8622c277444
selma-hayoun <selma.hayoun.caballero@gmail.com>
2022-03-28 19:15:32 +0200

    FirstCommit

---
+++ src/main/java/com/dam/kanpeki/KanpekiApplication.java
@@ -0,0 +1,13 @@
+package com.dam.kanpeki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanpekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanpekiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/kanpeki/KanpekiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.kanpeki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanpekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
