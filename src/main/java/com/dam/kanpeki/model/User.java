f09472bcc418e6ff725cbd4182b759ea26c7832e
selma-hayoun <selma.hayoun.caballero@gmail.com>
2022-04-11 08:23:46 +0200

    POJOs review

--- src/main/java/com/dam/kanpeki/model/User.java
+++ src/main/java/com/dam/kanpeki/model/User.java
@@ -1,6 +1,8 @@
 package com.dam.kanpeki.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -16,6 +18,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -36,11 +40,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 //public class User implements UserDetails {
-public class User {
-
-//	@Id
-//	@GeneratedValue
-//	private Long id;
+public class User implements Serializable {
 
 	/**
 	 * serialVersionUID
@@ -63,7 +63,8 @@ public class User {
 
 	private String URLimage;
 
-	private LocalDateTime birthday;
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
 
 	private String city;
 
@@ -80,86 +81,5 @@ public class User {
 	@OneToMany(fetch = FetchType.LAZY)
 	@JoinColumn(name = "userId")
 	private Set<Result> results;
-
-//	@Override
-//	public Collection<? extends GrantedAuthority> getAuthorities() {
-//		return roles.stream().map(ur -> new SimpleGrantedAuthority("ROLE_" + ur.name())).collect(Collectors.toList());
-//	}
-//
-//	// MÉTODOS USERDETAILS
-//
-//	@Override
-//	public String getUsername() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public boolean isAccountNonExpired() {
-//		// Para gestionar la expiración de cuentas
-//		return false;
-//	}
-//
-//	@Override
-//	public boolean isAccountNonLocked() {
-//		// Para gestionar el bloqueo de cuentas
-//		return false;
-//	}
-//
-//	@Override
-//	public boolean isCredentialsNonExpired() {
-//		// Para gestionar la expiración de cuentas
-//		return false;
-//	}
-//
-//	@Override
-//	public boolean isEnabled() {
-//		// Para gestionar el bloqueo de cuentas
-//		return false;
-//	}
-//
-//	@Override
-//	public String getPassword() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-
-//	/**
-//	 * No vamos a gestionar la expiración de cuentas. De hacerse, se tendría que dar
-//	 * cuerpo a este método
-//	 */
-//	@Override
-//	public boolean isAccountNonExpired() {
-//		return true;
-//	}
-//
-//	/**
-//	 * No vamos a gestionar el bloqueo de cuentas. De hacerse, se tendría que dar
-//	 * cuerpo a este método
-//	 */
-//	@Override
-//	public boolean isAccountNonLocked() {
-//		return true;
-//	}
-//
-//	/**
-//	 * No vamos a gestionar la expiración de cuentas. De hacerse, se tendría que dar
-//	 * cuerpo a este método
-//	 */
-//
-//	@Override
-//	public boolean isCredentialsNonExpired() {
-//		return true;
-//	}
-//
-//	
-//	/**
-//	 * No vamos a gestionar el bloqueo de cuentas. De hacerse, se tendría que dar
-//	 * cuerpo a este método
-//	 */	
-//	@Override
-//	public boolean isEnabled() {
-//		return true;
-//	}
 
 }
