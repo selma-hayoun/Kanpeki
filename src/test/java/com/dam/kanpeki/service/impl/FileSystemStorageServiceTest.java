e95be8ec1cd56c8c8b78e64c70e47f76cf332396
selma-hayoun <selma.hayoun.caballero@gmail.com>
2022-05-10 18:52:27 +0200

    Logs

--- src/test/java/com/dam/kanpeki/service/impl/FileSystemStorageServiceTest.java
+++ src/test/java/com/dam/kanpeki/service/impl/FileSystemStorageServiceTest.java
@@ -1,15 +1,17 @@
 package com.dam.kanpeki.service.impl;
 
-//import java.nio.file.Path;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.BeforeEach;
 
 public class FileSystemStorageServiceTest {
 
-//	private Path rootLocation;
+	private Path rootLocation;
 
 	@BeforeEach
 	public void setup() {
+		rootLocation = Paths.get("./upload-dir");
 
 	}
 
